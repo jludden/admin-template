5f1a6eea27bed1fa26187b409b404cbdaf8d8f9e
jludden <jasonludden@gmail.com>
2018-03-11 21:30:19 +0800

    adding users screen and updating posts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { jsonServerRestClient, Admin, Resource } from "admin-on-rest";
+import { jsonServerRestClient, Admin, Resource, Delete } from "admin-on-rest";
 
-import { PostList } from "./posts";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
 
 const App = () => (
-  <Admin
-    restClient={jsonServerRestClient("https://jsonplaceholder.typicode.com")}
-  >
-    <Resource name="posts" list={PostList} />
+  <Admin restClient={jsonServerRestClient(
+        "https://jsonplaceholder.typicode.com")}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} />
+    <Resource name="users" list={UserList} />
   </Admin>
 );
 
