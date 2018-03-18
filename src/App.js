f7532e72d08f8639aeb897888f03eee859838ca5
jludden <jasonludden@gmail.com>
2018-03-18 18:27:00 +0800

    css grid + mediaquery

--- src/App.js
+++ src/App.js
@@ -9,10 +9,26 @@ class App extends Component {
         <div>
           <Route path="/admin/" component={adminConsole}/>
           <Route exact={true} path="/" render={() => (
-            <div>
-              <h1> Welcome to the Hangzhou Dylan Tech demo </h1>
-              <Link to={'/admin/'}> Admin console </Link>
-            </div>)}/>
+            <div class="wrapper">
+              <header class="box main-header">
+                  <h1> Welcome to the Hangzhou Dylan Tech demo </h1>
+                  <p class="login"><Link to={'/admin/'}> Admin console </Link></p>
+              </header>
+              <nav class="box main-nav">
+              <ul>
+                  <li><a href="">Nav 1</a></li>
+                  <li><a href="">Nav 2</a></li>
+                  <li><a href="">Nav 3</a></li>
+              </ul>
+              </nav>
+              <article class="box content">
+                  <h1>Main article area</h1>
+                  <p>In this layout, we display the areas in source order for any screen less that 500 pixels wide. We go to a two column layout, and then to a three column layout by redefining the grid, and the placement of items on the grid.</p>
+              </article> 
+              <div class="box sidebar">Sidebar</div>
+              <footer class="box main-footer">The footer</footer>
+            </div>
+            )}/>
         </div>
      </Router>
     )
